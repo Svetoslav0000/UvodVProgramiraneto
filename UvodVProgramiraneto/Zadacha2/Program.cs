9886e950c9f38f9aab585ae7c43df69b13bb0a0a
Svetoslav0000 <152181432+Svetoslav0000@users.noreply.github.com>
2023-11-27 15:01:55 +0200

    Zadacha

---
+++ UvodVProgramiraneto/Zadacha2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vuvedi chislo za den ot sedmicata");
+            int n = int.Parse(Console.ReadLine());
+            switch(n)
+            {
+                case 1 :Console.WriteLine("Ponedelnik");break;
+                case 2: Console.WriteLine("Vtornik"); break;
+                case 3: Console.WriteLine("Srqda"); break;
+                case 4: Console.WriteLine("Chetvurtuk"); break;
+                case 5: Console.WriteLine("Petuk"); break;
+                case 6: Console.WriteLine("Subota"); break;
+                case 7: Console.WriteLine("Nedelq"); break;
+                    default: Console.WriteLine("Nevalidni vhodni danni");break;
+
+            }
+        }
+    }
+}
